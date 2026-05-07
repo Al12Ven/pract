a93d430dc812c6d3ee7db15d404b118079e159cb
Yakov Sadovkiy and Ivanova Milana <j85984168@gmail.com>
2026-05-07 14:01:44 +0300

    new

--- js/script.js
+++ js/script.js
@@ -1,4 +1,27 @@
 
-function openwindow(){
+// function openwindow(){
+//     window.open('search.html')
+// }
+
+
+function handleFormSubmit(event) {
+  // Просим форму не отправлять данные самостоятельно
+  event.preventDefault()
+//   console.log('Отправка!')
     window.open('search.html')
 }
+
+const applicantForm = document.getElementById('form_reg')
+applicantForm.addEventListener('submit', handleFormSubmit)
+
+// function serializeForm(formNode) {
+//   console.log(formNode.elements)
+// }
+
+// function handleFormSubmit(event) {
+//   event.preventDefault()
+//   serializeForm(applicantForm)
+// }
+
+// const applicantForm = document.getElementById('form_reg')
+// applicantForm.addEventListener('submit', handleFormSubmit)
